e0d13b595b03787edc75d7cd4baeb35365e49fcb
Christian Montoya <christian.montoya@systemproject.cl>
2023-07-26 22:43:29 -0400

    feat: se agrega card base a HorizontalBookingCard y se crea screen BookingPlaceScreen

--- src/components/HorizontalBookingCard.js
+++ src/components/HorizontalBookingCard.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        margin: 20,
+    },
+    containerImg: {
+        backgroundColor: 'red',
+        width:  240,
+        height: 340,
+        borderRadius: 40,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'black',
+        fontStyle: "normal",
+        marginVertical: 10,
+    }
+});
 const HorizontalBookingCard = ({data}) => {
+    const navigation = useNavigation();
     return (
         <View>
             <FlatList
                 horizontal
                 data={data}
-                renderItem={({item}) => <Text>{item.name}</Text>}
-                keyExtractor={item=> item.id}/>
+                renderItem={({item}) =>
+                    <View style={styles.container}>
+                        <TouchableOpacity onPress={() => navigation.navigate('BookingPlace')}>
+                            <View style={styles.containerImg}/>
+                        </TouchableOpacity>
+                        <Text style={styles.title} >{item.name}</Text>
+                    </View>
+                }
+                keyExtractor={item => item.id}/>
         </View>
     )
 }
--- src/routes/RouterNavigationStack.js
+++ src/routes/RouterNavigationStack.js
@@ -4,14 +4,16 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from '../screens/login/LoginScreen';
 import RegisterScreen from '../screens/login/RegisterScreen';
 import HomeScreen from "../screens/home/HomeScreen";
+import BookingPlaceScreen from "../screens/home/BookingPlaceScreen";
 const RouterNavigationStack = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={'Home'}>
+      <Stack.Navigator initialRouteName={'BookingPlace'}>
         <Stack.Screen name={'Login'} component={LoginScreen} />
           <Stack.Screen name={'Register'} component={RegisterScreen} />
           <Stack.Screen name={'Home'} component={HomeScreen} />
+          <Stack.Screen name={'BookingPlace'} component={BookingPlaceScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
